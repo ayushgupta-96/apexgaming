1d2c3f1c8a80923850431743748b8aa08c0e8319
Ayush Kumar Gupta <akg797975@gmail.com>
2026-09-15 18:26:59 +0530

    Use new uploaded game artwork and fixed card dimensions

--- src/main/resources/static/js/home-games.js
+++ src/main/resources/static/js/home-games.js
@@ -1,17 +1,17 @@
 const HOME_GAMES = [
   { name: 'Aviator', type: 'aviator', image: '/images/games/aviator.webp' },
-  { name: 'Colour Prediction', type: 'colour', image: '/images/games/colour.svg' },
-  { name: 'Ludo', type: 'ludo', image: '/images/games/ludo.svg' },
-  { name: 'Mines', type: 'mines', image: '/images/games/mines.svg' },
-  { name: 'Fruit 777', type: 'fruit', image: '/images/games/fruit.svg' }
+  { name: 'Fruit 777', type: 'fruit', image: '/images/games/fruit.webp' },
+  { name: 'Ludo', type: 'ludo', image: '/images/games/ludo.webp' },
+  { name: 'Colour Prediction', type: 'colour', image: '/images/games/colour.webp' },
+  { name: 'Mines', type: 'mines', image: '/images/games/mines.svg' }
 ];
 
 function renderHomeGames() {
   const target = document.getElementById('categoryGames');
   if (!target) return;
   target.innerHTML = HOME_GAMES.map(game => `
-    <button class="catalog-card ${game.type}" onclick="switchTab('${game.type === 'aviator' ? 'aviator' : game.type === 'colour' ? 'colour' : game.type === 'ludo' ? 'ludo' : 'games'}')">
-      <img class="catalog-image" src="${game.image}" alt="${game.name}" loading="eager">
+    <button class="catalog-card ${game.type}" onclick="switchTab('${game.type === 'aviator' ? 'aviator' : game.type === 'colour' ? 'colour' : game.type === 'ludo' ? 'ludo' : game.type === 'fruit' ? 'games' : 'games'}')">
+      <img class="catalog-image" src="${game.image}" alt="${game.name}" loading="eager" width="333" height="450">
       <b>${game.name}</b>
     </button>
   `).join('');
